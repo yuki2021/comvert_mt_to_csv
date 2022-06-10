19b43dcbfab4ed0c9e9f6004610872c00210e0e0
yuki2021 <ordinary.plusminus1@gmail.com>
2022-06-10 20:59:18 +0900

    [update]オブジェクト指向に変更

--- comvert_csv.php
+++ comvert_csv.php
@@ -1,80 +1,87 @@
 <?php
 
-$blog_url = 'https://www.ituki-yu2.net/entry/';
 
-$registDate = [];
-$updateDate = [];
-$titleArr = [];
-$urlArr = [];
+class ComvertMtToCSV {
 
-function file_load() {
+    public $blog_url = '';
+    public $read_path = '';
+    public $write_path = '';
 
-    global $registDate, $updateDate, $titleArr, $urlArr;
+    private $registDate = [];
+    private $updateDate = [];
+    private $titleArr = [];
+    private $urlArr = [];
 
-    $fp = fopen('./text.txt','r');
-    if ($fp) {
-        while (($buffer = fgets($fp)) !== false) {
+    function file_load() {
+
+        $fp = fopen($this->read_path,'r');
+        if ($fp) {
+            while (($buffer = fgets($fp)) !== false) {
+                $temp_text = '';
+                if(preg_match('/^DATE:(.*)$/', $buffer) == 1) {
+                    $temp_text = $this->trim_data_text($buffer);
+                    $this->registDate[] = $temp_text;
+                    $this->updateDate[] = $temp_text;
+                    continue;
+                }
+                if(preg_match('/^BASENAME:(.*)$/', $buffer) == 1) {
+                    $temp_text = $this->trim_url_text($buffer);
+                    $this->urlArr[] = $temp_text;
+                    continue;
+                }
+                if(preg_match('/^TITLE:(.*)$/', $buffer) == 1) {
+                    $temp_text = $this->trim_title_text($buffer);
+                    $this->titleArr[] = $temp_text;
+                    continue;
+                }
+            }
+            fclose($fp);
+            
+        }
+    }
+
+    function file_read() {
+
+        $fp2 = fopen($this->write_path, 'c+');
+        foreach($this->registDate as $key => $loop) {
             $temp_text = '';
-            if(preg_match('/^DATE:(.*)$/', $buffer) == 1) {
-                $temp_text = trim_data_text($buffer);
-                $registDate[] = $temp_text;
-                $updateDate[] = $temp_text;
-                continue;
-            }
-            if(preg_match('/^BASENAME:(.*)$/', $buffer) == 1) {
-                $temp_text = trim_url_text($buffer);
-                $urlArr[] = $temp_text;
-                continue;
-            }
-            if(preg_match('/^TITLE:(.*)$/', $buffer) == 1) {
-                $temp_text = trim_title_text($buffer);
-                $titleArr[] = $temp_text;
-                continue;
+            $temp_text = '"'. $loop .'","'. $this->updateDate[$key] .'","'.
+                    $this->titleArr[$key] .'","'. $this->urlArr[$key] .'"'. ",\n";
+            if(fwrite($fp2, $temp_text) == false) {
+                print("ファイル書き込みに失敗しました:". $temp_text);
+                break;
             }
         }
-        fclose($fp);
-        
+        fclose($fp2);
+    }
+
+    function trim_data_text($date) {
+        $temp = trim($date, 'DATE: ');
+        $temp = trim($temp);
+        return $temp;
+    }
+
+    function trim_url_text($url) {
+
+        global $blog_url;
+
+        $temp = trim($url, 'BASENAME: ');
+        $temp2 = $blog_url . $temp;
+        $temp2 = trim($temp2);
+        return $temp2;
+    }
+
+    function trim_title_text($title) {
+        $temp = trim($title, 'TITLE: ');
+        $temp = trim($temp);
+        return $temp;
     }
 }
 
-function file_read() {
-
-    global $registDate, $updateDate, $titleArr, $urlArr;
-
-    $fp2 = fopen('./write_new_csv.csv', 'c+');
-    foreach($registDate as $key => $loop) {
-        $temp_text = '';
-        $temp_text = '"'. $loop .'","'. $updateDate[$key] .'","'. $titleArr[$key] .'","'. $urlArr[$key] .'"'. ",\n";
-        if(fwrite($fp2, $temp_text) == false) {
-            print("ファイル書き込みに失敗しました:". $temp_text);
-            break;
-        }
-    }
-    fclose($fp2);
-}
-
-function trim_data_text($date) {
-    $temp = trim($date, 'DATE: ');
-    $temp = trim($temp);
-    return $temp;
-}
-
-function trim_url_text($url) {
-
-    global $blog_url;
-
-    $temp = trim($url, 'BASENAME: ');
-    $temp2 = $blog_url . $temp;
-    $temp2 = trim($temp2);
-    return $temp2;
-}
-
-function trim_title_text($title) {
-    $temp = trim($title, 'TITLE: ');
-    $temp = trim($temp);
-    return $temp;
-}
-
-file_load();
-file_read();
+$obj = new ComvertMtToCSV();
+$obj->blog_url = 'https://www.ituki-yu2.net/entry/';
+$obj->read_path = './text.txt';
+$obj->write_path = './write_new_csv.csv';
+$obj->file_load();
+$obj->file_read();
 exit();
